7f6e8ef5aa3aaa57d94218088a3d7b69b7137ace
Hai Tang <402696415@qq.com>
2018-05-28 18:55:35 -0700

    slice, plot multiple, normalize

--- mc1.py
+++ mc1.py
@@ -1,20 +1,65 @@
 import pandas as pd
+import os
+import matplotlib.pyplot as plt
 
-def test_run():
-    start_date = '2012-01-22'
-    end_date = '2012-01-26'
-    dates = pd.date_range(start_date, end_date)
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
 
-    #Create an empty dataframe
-    df1 = pd.DataFrame(index=dates)
+    for symbol in symbols:
+        # TODO: Read and join data for each symbol
+        df_temp = pd.read_csv("data/{}.csv".format(symbol), 
+                              index_col="Date",
+                              parse_dates=True,
+                              usecols=['Date', 'Adj Close'],
+                              na_values='nan')
+        df_temp = df_temp.rename(columns={'Adj Close':symbol})
+        df = df.join(df_temp)
+        if symbol == 'SPY':
+            df = df.dropna()
 
-    #Read SPY data into temporary dataframe
-    dfSPY = pd.read_csv("data/SPY.csv", index_col="Date", parse_dates=True)
+    return df
 
-    #Join the two dataframes using DataFrame.join()
-    df1 = df1.join(dfSPY)
-    print df1
+def plot_selected(df, columns, start_index, end_index):
+    plot_data(df.ix[start_index:end_index, columns], title="Selected data")
+
+def normalize_data(df):
+    '''Normalize stock prices using the first row of the df.'''
+    #df.ix[0, :] will give us the first row
+    return df / df.ix[0, :]
+
+def plot_data(df, title="Stock prices"):
+    '''Plot stock prices'''
+    ax = df.plot(title=title, fontsize=8)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
+
+def test_run_mc1p2():
+    # Define a date range
+    dates = pd.date_range('2010-01-01', '2010-12-31')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
     
+    # Get stock data
+    df = get_data(symbols, dates)
+    
+    # Slice by row range (dates) using DataFrame.ix[] selector
+    # print df.ix['2010-01-01':'2010-01-31']
+    
+    # Slice by column (symbols)
+    # print df['GOOG']
+    # print df[['IBM', 'GLD']]
+
+    # Slice by row and column
+    print df.ix['2010-03-10':'2010-03-15', ['IBM', 'GLD']]
+    #plot_data(df)
+    #plot_selected(df, ['SPY', 'IBM'], '2010-03-01', '2010-04-01')
+    plot_data(normalize_data(df))
+
 
 if __name__ == '__main__':
-    test_run()
+    test_run_mc1p2()
