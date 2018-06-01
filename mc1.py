50cb2f58e4e35ee5d7506fd793b6061696e58a2a
Hai Tang <402696415@qq.com>
2018-05-31 22:39:49 -0700

    optimizations

--- mc1.py
+++ mc1.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import scipy.optimize as spo
 
 def get_max_close(symbol):
     '''Return the maximum closing value for stock indicated by symbol.'''
@@ -98,6 +99,71 @@ def fill_missing_values(df_data):
     df_data.fillna(method="bfill", inplace=True) # backward fill
     
     return df_data
+
+def f(X):
+    """Given a scalar X, return some value"""
+    Y = (X - 1.5)**2 + 0.5
+    print "X = {}, Y = {}".format(X, Y)
+    return Y
+
+def error(line, data):
+    """ error between given line model and observed data. 
+
+    Parameters
+    ------------
+    line: tuple/list/array ((C0, C1)) where C0 is slope and C1 is Y-intercept
+    data: 2D array where each row is a point (x, y)
+
+    Returns error as a single real value
+    """
+    # Metric: Sum of squared Y-axis differences
+    err = np.sum((data[:, 1] - (line[0] * data[:, 0] + line[1])) ** 2)
+    return err
+
+def error_poly(C, data):
+    """ error between given polynomial and observed data. 
+
+    Parameters
+    ------------
+    line: numpy.poly1d object or equivalent array representing polynomial coefficients
+    data: 2D array where each row is a point (x, y)
+
+    Returns error as a single real value
+    """
+    # Metric: Sum of squared Y-axis differences
+    err = np.sum((data[:, 1] - np.polyval(C, data[:, 0])) ** 2)
+    return err
+
+def fit_line(data, error_func):
+    """ Fit a line to given data, using a supplied error function
+
+    Parameters
+    -----------
+    data: 2D array where each row is a point (X0, Y)
+    error_func: function that computes the error function.
+    """
+    # Generate initial guess for line model
+    l = np.float32([0, np.mean(data[:, 1])]) # slope = 0, intercept = mean y
+
+    # Call optimizer to minimize err function
+    result = spo.minimize(error_func, l, args=(data,), method='SLSQP', options={'display': True})
+    return result.x
+
+def fit_poly(data, error_func, degree=3):
+    """ Fit a polynomial to given data, using a supplied error function
+
+    Parameters
+    -----------
+    data: 2D array where each row is a point (X0, Y)
+    error_func: function that computes the error function.
+    """
+    # Generate initial guess for line model
+    Cguess = np.poly1d(np.ones(degree + 1, dtype=np.float32))
+
+    # Call optimizer to minimize err function
+    result = spo.minimize(error_func, Cguess, args=(data,), method='SLSQP', options={'display': True})
+    return np.poly1d(result.x)
+
 
 def test_run_mc1p1():
     df = pd.read_csv("data/AAPL.csv")
@@ -351,10 +417,47 @@ def test_run_mc1p6():
     # Calculate correlation coefficient
     print daily_returns.corr(method='pearson')
 
+def test_run_mc1p8():
+    # Xguess = 2.0
+    # min_result = spo.minimize(f, Xguess, method='SLSQP', options={'disp': True})
+    # print "Minima found at:"
+    # print "X = {}, Y = {}".format(min_result.x, min_result.fun)
+
+    # # Plot function values, mark minima
+    # Xplot = np.linspace(0.5, 2.5, 21)
+    # Yplot = f(Xplot)
+    # plt.plot(Xplot, Yplot)
+    # plt.plot(min_result.x, min_result.fun, 'ro')
+    # plt.title("Minima of an objective function")
+    # plt.show()
+
+    # Define original line
+    l_orig = np.float32([4, 2])
+    print "Original line: C0 = {}, C1 = {}".format(l_orig[0], l_orig[1])
+    Xorig = np.linspace(0, 10, 21)
+    Yorig = l_orig[0] * Xorig + l_orig[1]
+    plt.plot(Xorig, Yorig, 'b--', linewidth=2.0, label="Original line")
+
+    # Generate nosidy data point
+    noise_sigma = 3.0
+    noise = np.random.normal(0, noise_sigma, Yorig.shape)
+    data = np.asarray([Xorig, Yorig + noise]).T
+    plt.plot(data[:, 0], data[:, 1], 'go', label="Data points")
+
+    # Try to fit a line to this data
+    # l_fit = fit_line(data, error)
+    l_fit = fit_poly(data, error)
+    print "Fitted line: C0={}, C1={}".format(l_fit[0], l_fit[1])
+    plt.plot(data[:, 0], l_fit[0] * data[:, 0]+l_fit[1], 'r--', linewidth=2.0, label="Fit line")
+
+    plt.show()
+
+
 if __name__ == '__main__':
     # test_run_mc1p1()
     # test_run_mc1p2()
     # test_run_mc1p3()
     # test_run_mc1p4()
     # test_run_mc1p5()
-    test_run_mc1p6()
+    # test_run_mc1p6()
+    test_run_mc1p8()
